9043c63e49c7be4351d555dbdce074f37ad2aa47
RihardsGedrovics <125249238+RihardsGedrovics@users.noreply.github.com>
2023-03-19 20:04:52 +0000

    testa

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ def parallel_processing(num_threads, num_jobs, job_times):
     next_free_time = [0] * num_threads
     threads = list(range(num_threads))
     for job_index, job_time in enumerate(job_times):
-        next_thread gjhb= min(threads, key=lambda i: next_free_time[i])
+        next_thread = min(threads, key=lambda i: next_free_time[i])
         output.append((next_thread, next_free_time[next_thread]))
         next_free_time[next_thread] += job_time
     return output
